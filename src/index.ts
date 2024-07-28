014f77577c2ab5fc0a1bf73373e67ada172b0ab9
yocopk <yocopk@gmail.com>
2024-07-28 13:11:30 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+})
